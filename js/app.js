3098cbce1013f52ee8c49f6346c66775d7dc14f0
James Bremner <j.bremner@ymail.com>
2023-11-29 16:23:28 +0000

    add search functionality

--- js/app.js
+++ js/app.js
@@ -2,5 +2,18 @@
 
 baguetteBox.run('.gallery-container');
 
-//add the search plug-in
-const search = new Filter('search', 'data-caption');
+const userInput = document.querySelector('#image-search');
+
+const searchForInput = (event) => {
+  let message = event.target.value.toLowerCase();
+  const images = document.querySelectorAll("a[data-caption]");
+  images.forEach((element)=> {
+   if (element.dataset.caption.toLowerCase().includes(message)) {
+       element.style.display = 'block';
+   } else {
+       element.style.display = 'none';
+   }
+  })
+}
+userInput.addEventListener('keyup', searchForInput);
+
