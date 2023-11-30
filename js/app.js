66233351cd258140a441914d5e4a01c0d1500034
James Bremner <j.bremner@ymail.com>
2023-11-30 11:13:25 +0000

    add comments

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
 //run the gallery plug-in
-
 baguetteBox.run('.gallery-container');
 
+//target the search input
 const userInput = document.querySelector('#image-search');
 
 const searchForInput = (event) => {
+  //get the user input
   let message = event.target.value.toLowerCase();
+  //target all images that have a data-caption attribute
   const images = document.querySelectorAll("a[data-caption]");
-  images.forEach((element)=> {
-   if (element.dataset.caption.toLowerCase().includes(message)) {
-       element.style.display = 'block';
+  //loop through images and check if the caption contains the user input
+  //display the image if it matches the input
+  //else hide the image
+  images.forEach((thumbnail)=> {
+    let captionText = thumbnail.dataset.caption.toLowerCase();
+   if (captionText.includes(message)) {
+       thumbnail.style.display = 'block';
    } else {
-       element.style.display = 'none';
+       thumbnail.style.display = 'none';
    }
   })
 }
+//add event listener to the input element
 userInput.addEventListener('keyup', searchForInput);
 
